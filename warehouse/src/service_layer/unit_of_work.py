ba7bd3acceb2fddcf10579390f45c7e245738f57
AdamMisiak <adammisiak3@gmail.com>
2022-04-25 17:10:20 +0200

    Added unit of work patter

---
+++ warehouse/src/service_layer/unit_of_work.py
@@ -0,0 +1,60 @@
+# pylint: disable=attribute-defined-outside-init
+from __future__ import annotations
+import abc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+
+from src.adapters import repository
+
+
+class AbstractUnitOfWork(abc.ABC):
+    '''
+    AbstractUnitOfWork is the Unit of Work patten.
+    UoW pattern is abstraction over the idea of atomic operations. 
+    '''
+
+    def __enter__(self) -> AbstractUnitOfWork:
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    def commit(self):
+        self._commit()
+
+    @abc.abstractmethod
+    def _commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        f"postgresql://admin:admin@localhost:5432/micro_warehouse",
+        isolation_level="REPEATABLE READ",
+    )
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()  # type: Session
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def _commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
