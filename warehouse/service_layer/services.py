d3b8d74156fbf840f25813823bba37f81d7a58a7
AdamMisiak <adammisiak3@gmail.com>
2022-04-25 18:00:21 +0200

    Changed structure of warehouse service

---
+++ warehouse/service_layer/services.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import Optional, TYPE_CHECKING
+from datetime import date
+
+from src.domain import model
+from src.domain.model import OrderLine
+
+if TYPE_CHECKING:
+    from . import unit_of_work
+
+
+class InvalidSku(Exception):
+    pass
+
+
+def add_batch(
+    ref: str, sku: str, qty: int, eta: Optional[date],
+    uow: unit_of_work.AbstractUnitOfWork,
+):
+    with uow:
+        uow.batches.append(model.Batch(ref, sku, qty, eta))
+        uow.commit()
+
