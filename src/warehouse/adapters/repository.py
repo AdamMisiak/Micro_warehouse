dc19d0cd0eece398f04a527bb3a73a547fc20721
AdamMisiak <adammisiak3@gmail.com>
2022-05-03 17:25:45 +0200

    Updated UoW and Repository

--- src/warehouse/adapters/repository.py
+++ src/warehouse/adapters/repository.py
@@ -12,22 +12,22 @@ class AbstractRepository(abc.ABC):
     def __init__(self):
         self.seen = set()  # type: Set[model.Product]
 
-    def add(self, product: model.Batch):
+    def add(self, product: model.Product):
         self._add(product)
         self.seen.add(product)
 
-    def get(self, sku) -> model.Batch:
+    def get(self, sku) -> model.Product:
         product = self._get(sku)
         if product:
             self.seen.add(product)
         return product
 
     @abc.abstractmethod
-    def _add(self, product: model.Batch):
+    def _add(self, product: model.Product):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def _get(self, sku) -> model.Batch:
+    def _get(self, sku) -> model.Product:
         raise NotImplementedError
 
 
@@ -45,4 +45,4 @@ class SqlAlchemyRepository(AbstractRepository):
         self.session.add(product)
 
     def _get(self, sku):
-        return self.session.query(model.Batch).filter_by(sku=sku).first()
+        return self.session.query(model.Product).filter_by(sku=sku).first()
