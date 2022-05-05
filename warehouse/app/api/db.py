9fa397b316c1a3cdf7c0a5fb987c90342c91ae58
AdamMisiak <adammisiak3@gmail.com>
2022-05-05 17:32:37 +0200

    Added connection to db

---
+++ warehouse/app/api/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import (Column, Integer, MetaData, String, Table, Date,
+                        create_engine, ARRAY)
+
+from databases import Database
+
+DATABASE_URL = 'postgresql://admin:admin@warehouse_db/warehouse_db'
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+batches = Table(
+    'batches',
+    metadata,
+    Column('ref', String(50), primary_key=True),
+    Column('sku', String(50)),
+    Column('qty', String(250)),
+    Column('eta', Date),
+
+)
+
+database = Database(DATABASE_URL)
