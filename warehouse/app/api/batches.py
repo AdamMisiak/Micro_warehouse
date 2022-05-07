c500dd3ee79251bd9f87eaa149012c235c920dec
AdamMisiak <adammisiak3@gmail.com>
2022-05-07 12:23:03 +0200

    Refactored POST batch endpoint

--- warehouse/app/api/batches.py
+++ warehouse/app/api/batches.py
@@ -1,23 +1,11 @@
-from typing import List
 from fastapi import Header, APIRouter
-from app.api.db import database, batches
 from app.api.models import Batch
-# from app.api import db_manager
+from app.api import db_manager
 
 batches_router = APIRouter()
 
-# @batches.get('/', response_model=List[MovieOut])
-# async def index():
-#     return await db_manager.get_all_movies()
 
 @batches_router.post('/', status_code=201)
 async def add_batch(payload: Batch):
-    await database.connect()
-    batch_ref = await database.execute(query=batches.insert().values(**payload.dict()))
-    print(batch_ref)
-    response = {
-        'ref': batch_ref,
-        **payload.dict()
-    }
-
-    return response
+    await db_manager.add_batch(payload)
+    return payload
