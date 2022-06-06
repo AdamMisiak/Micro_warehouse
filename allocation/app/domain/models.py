3eb349d5cb2bf8f559f9c046fa18748b087284ce
AdamMisiak <adammisiak3@gmail.com>
2022-06-06 17:15:25 +0200

    Added allocation method to domain model Batch

--- allocation/app/domain/models.py
+++ allocation/app/domain/models.py
@@ -5,16 +5,46 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
+class OrderLine(BaseModel):
+    # TODO: adding those should be another service - "ordering"?
+    id: int
+    sku: str
+    quantity: int
+
+    class Config:
+        orm_mode = True
+
+
 class Batch(BaseModel):
     # id: int
     sku: str
     reference: str
     quantity: int
     eta: Optional[date]
+    _allocations: Optional[set]
 
     # def allocate(self, line) -> str:
-    def allocate(self):
-        print("allocating")
+    # def allocate(self):
+    #     print("allocating")
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self.quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.quantity
 
     class Config:
         orm_mode = True
@@ -28,16 +58,6 @@ class Product(BaseModel):
     # def allocate(self, line) -> str:
     def allocate(self):
         print("allocating")
-
-    class Config:
-        orm_mode = True
-
-
-class OrderLine(BaseModel):
-    # TODO: adding those should be another service - "ordering"?
-    id: int
-    sku: str
-    quantity: int
 
     class Config:
         orm_mode = True
