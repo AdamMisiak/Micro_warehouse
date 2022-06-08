42e6292341627a43bfadb9bb36390abb7663b88e
AdamMisiak <adammisiak3@gmail.com>
2022-06-08 17:31:23 +0200

    Added SQLModel library

--- allocation/app/domain/models.py
+++ allocation/app/domain/models.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 
 from app.utils import exceptions
 from pydantic import BaseModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class OrderLine(BaseModel):
@@ -16,13 +17,38 @@ class OrderLine(BaseModel):
         orm_mode = True
 
 
-class Batch(BaseModel):
-    # id: int
-    sku: str
+# class Product(BaseModel):
+class Product(SQLModel, table=True):
+    sku: str = Field(default=None, primary_key=True)
+    version_number: Optional[int]
+    batches: List["Batch"] = Relationship(back_populates="product")
+
+    # heroes: List["Hero"] = Relationship(back_populates="team")
+
+    def allocate(self, line: OrderLine) -> str:
+        try:
+            batch = self.batches[0]
+            # batch = next(batch for batch in sorted(self.batches) if batch.can_allocate(line))
+            batch.allocate(line)
+            self.version_number += 1
+            return batch.reference
+        except StopIteration:
+            raise exceptions.OutOfStock(f"Out of stock for sku {line.sku}")
+
+    # class Config:
+    #     orm_mode = True
+
+
+# class Batch(BaseModel):
+class Batch(SQLModel, table=True):
+    id: int = Field(index=True, primary_key=True)
+    sku: str = Field(default=None, foreign_key="product.sku")
     reference: str
     quantity: int
     eta: Optional[date]
     _allocations: Optional[set]
+
+    product: Product = Relationship(back_populates="batches")
 
     def allocate(self, line: OrderLine):
         if self.can_allocate(line):
@@ -43,24 +69,5 @@ class Batch(BaseModel):
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.quantity
 
-    class Config:
-        orm_mode = True
-
-
-class Product(BaseModel):
-    sku: str
-    version_number: Optional[int]
-    batches: List[Batch]
-
-    def allocate(self, line: OrderLine) -> str:
-        try:
-            batch = self.batches[0]
-            # batch = next(batch for batch in sorted(self.batches) if batch.can_allocate(line))
-            batch.allocate(line)
-            self.version_number += 1
-            return batch.reference
-        except StopIteration:
-            raise exceptions.OutOfStock(f"Out of stock for sku {line.sku}")
-
-    class Config:
-        orm_mode = True
+    # class Config:
+    #     orm_mode = True
--- allocation/app/main.py
+++ allocation/app/main.py
@@ -1,12 +1,18 @@
 # pylint: disable=too-few-public-methods
-from app.adapters.orm import Base, engine
+# from app.adapters.orm import Base, engine
+from app.adapters.orm import create_db_and_tables, engine
 from app.routers import batches, order_lines, products
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
+SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()
 
 app.include_router(products.router)
 app.include_router(batches.router)
