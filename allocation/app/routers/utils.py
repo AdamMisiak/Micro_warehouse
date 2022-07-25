0c714a6eb277361fdef3b1da05c8291544cdec59
AdamMisiak <adammisiak3@gmail.com>
2022-07-25 17:03:42 +0200

    Created separated router for utils

--- allocation/app/routers/utils.py
+++ allocation/app/routers/utils.py
@@ -1,10 +1,10 @@
 # pylint: disable=C0103
 # invalid-name
-from typing import List
 
+import localstack_client.session as boto3
 from app.database import get_db
-from app.domain import models, schemas
-from fastapi import APIRouter, Depends, HTTPException
+from app.utils import settings
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 router = APIRouter(
@@ -12,3 +12,15 @@ router = APIRouter(
     tags=["Utils"],
     responses={404: {"description": "Not found"}},
 )
+
+
+@router.get("/messages")
+def read_messages():
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    queue = sqs_resource.get_queue_by_name(QueueName=settings.QUEUE_NAME)
+    messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
+    for message in messages:
+        print(f"Received message: {message.message_id}, {message.body}, {message.message_attributes}")
+        # message.delete()
+        # print(message.delete())
+    return "test"
