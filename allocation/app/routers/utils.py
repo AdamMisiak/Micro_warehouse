fe7b4a5bad817506a71f37c3a94cc4f4db2b699a
AdamMisiak <adammisiak3@gmail.com>
2022-07-28 16:22:26 +0200

    Added deleting all messages

--- allocation/app/routers/utils.py
+++ allocation/app/routers/utils.py
@@ -19,9 +19,25 @@ def read_messages():
     sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
     queue = sqs_resource.get_queue_by_name(QueueName=settings.QUEUE_NAME)
     messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
-    results = [{"body": message.body, "attributes": message.message_attributes} for message in messages]
+    results = [
+        {"id": message.message_id, "body": message.body, "attributes": message.message_attributes}
+        for message in messages
+    ]
     # for message in messages:
     #     print(f"Received message: {message.message_id}, {message.body}, {message.message_attributes}")
     # message.delete()
     # print(message.delete())
+    return results
+
+
+@router.delete("/messages", response_model=List[schemas.Message])
+def delete_all_messages():
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    queue = sqs_resource.get_queue_by_name(QueueName=settings.QUEUE_NAME)
+    queue.purge()
+    messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
+    results = [
+        {"id": message.message_id, "body": message.body, "attributes": message.message_attributes}
+        for message in messages
+    ]
     return results
