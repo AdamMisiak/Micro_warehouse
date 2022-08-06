23726c667d3c2163cd9d366fc9edd66b126b9507
AdamMisiak <adammisiak3@gmail.com>
2022-08-06 08:14:06 +0200

    Added domain models and schemas to storage

---
+++ storage/app/domain/schemas.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Batches
+class BatchBase(BaseModel):
+    sku: str
+    reference: str
+    quantity: int
+    eta: Optional[datetime]
+
+
+class BatchCreate(BatchBase):
+    pass
+
+
+class Batch(BatchBase):
+    id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+# Orders
+class OrderBase(BaseModel):
+    sku: str
+    quantity: int
+
+
+class OrderCreate(OrderBase):
+    pass
+
+
+class Order(OrderBase):
+    id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class OrderWithBatch(Order):
+    batch: Optional[Batch] = None
