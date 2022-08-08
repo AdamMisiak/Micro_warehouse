4f164177a20730117e14d6bf699ca75a52ef9ce3
AdamMisiak <adammisiak3@gmail.com>
2022-08-08 07:00:51 +0200

    Added utils to storage

--- storage/app/main.py
+++ storage/app/main.py
@@ -1,5 +1,5 @@
 from app.domain.models import Base
-from app.routers import batches
+from app.routers import utils
 from fastapi import FastAPI
 
 from .database import engine
@@ -9,4 +9,4 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-app.include_router(batches.router)
+app.include_router(utils.router)
---
+++ storage/app/routers/utils.py
@@ -0,0 +1,67 @@
+# pylint: disable=C0103, W0703
+# invalid-name, broad-except
+
+from typing import List
+
+import localstack_client.session as boto3
+from app.domain import schemas
+from app.utils import settings
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/api/v1/utils",
+    tags=["Utils"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/queue/{queue_name}")
+def read_queue(queue_name: str):
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    try:
+        queue = sqs_resource.get_queue_by_name(QueueName=queue_name)
+        return {"url": queue.url, "attributes": queue.attributes}
+    except Exception as exc:
+        return {"error": "queue_does_not_exist", "msg": exc}
+
+
+@router.get("/queues", response_model=List[schemas.Queue])
+def read_queues():
+    sqs_client = boto3.client("sqs", region_name=settings.REGION)
+    queues = sqs_client.list_queues().get("QueueUrls")
+    results = [{"url": queue} for queue in queues]
+    return results
+
+
+@router.get("/messages", response_model=List[schemas.Message])
+def read_messages(queue: str = "micro-warehouse-external-queue"):
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    queue = sqs_resource.get_queue_by_name(QueueName=queue)
+    messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
+    results = [
+        {"id": message.message_id, "body": message.body, "attributes": message.message_attributes}
+        for message in messages
+    ]
+    return results
+
+
+@router.delete("/messages", response_model=List[schemas.Message])
+def delete_all_messages(queue: str = "micro-warehouse-external-queue"):
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    queue = sqs_resource.get_queue_by_name(QueueName=queue)
+    queue.purge()
+    messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
+    results = [
+        {"id": message.message_id, "body": message.body, "attributes": message.message_attributes}
+        for message in messages
+    ]
+    return results
+
+
+@router.post("/queues", response_model=schemas.Queue)
+def create_queue(queue: schemas.QueueCreate):
+    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
+    queue = sqs_resource.create_queue(
+        QueueName=queue.name, Attributes={"DelaySeconds": str(queue.delay), "VisibilityTimeout": str(queue.visibility)}
+    )
+    return queue
