5d00a1d93960c9bd3fe19797f5e54cbbe134bc71
AdamMisiak <adammisiak3@gmail.com>
2022-08-08 06:38:13 +0200

    Added batches endpoint to storage

--- storage/app/main.py
+++ storage/app/main.py
@@ -1,4 +1,5 @@
 from app.domain.models import Base
+from app.routers import batches
 from fastapi import FastAPI
 
 from .database import engine
@@ -8,6 +9,4 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-# app.include_router(batches.router)
-# app.include_router(orders.router)
-# app.include_router(utils.router)
+app.include_router(batches.router)
