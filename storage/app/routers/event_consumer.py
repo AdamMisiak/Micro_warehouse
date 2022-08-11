9407828dc9dd54613a0abe8757c41f2af0a008e8
AdamMisiak <adammisiak3@gmail.com>
2022-08-11 06:48:44 +0200

    Upgraded event consumer

--- storage/app/routers/event_consumer.py
+++ storage/app/routers/event_consumer.py
@@ -1,17 +1,22 @@
 import localstack_client.session as boto3
-from app.utils import settings
+
+# import boto3
+# from app.utils import settings
 
 
 def main():
     print("Event consumer started")
-    sqs_resource = boto3.resource("sqs", region_name=settings.REGION)
-    queue = sqs_resource.get_queue_by_name(QueueName=settings.QUEUE_NAME)
+    sqs_resource = boto3.resource("sqs", region_name="eu-west-1")
+    queue = sqs_resource.get_queue_by_name(QueueName="micro-warehouse-external-queue")
     messages = queue.receive_messages(MessageAttributeNames=["All"], MaxNumberOfMessages=10, WaitTimeSeconds=1)
     results = [
         {"id": message.message_id, "body": message.body, "attributes": message.message_attributes}
         for message in messages
     ]
-    print(results)
+    for message in results:
+        print(message)
+        # TODO add sending email
+        # TODO why messages are in queue event after receiving?
 
 
 if __name__ == "__main__":
