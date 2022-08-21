09200121b8c3439fd5cb493b4e40e54233e76a8f
AdamMisiak <adammisiak3@gmail.com>
2022-08-21 13:28:18 +0200

    Added get orders test

--- allocation/app/tests/unit/test_orders.py
+++ allocation/app/tests/unit/test_orders.py
@@ -10,3 +10,14 @@ def test_create_order(client):
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
     assert response.json() == response_body
+
+
+def test_get_orders(client, order):
+    response = client.get("/api/v1/orders")
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == 1
+    assert response.json()[0].get("id") == 1
+    assert response.json()[0].get("sku") == "BIG-TABLE"
+    assert response.json()[0].get("quantity") == 10
